f22ea91dca8953365f89abc24ed4d0b245223f23
olpsm <olpsm@mail.ru>
2021-12-19 21:04:59 +0300

    Initial commit

--- 1.JavaSyntax/src/com/javarush/task/task01/task0101/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0101/Solution.java
@@ -1,12 +1,12 @@
-package com.javarush.task.task01.task0101;                                                                                                                                                      
-                                                                                                                                                      
-/*                                                                                                                                                       
-Я думаю, быть программистом - это круто                                                                                                                                                      
-*/                                                                                                                                                      
-                                                                                                                                                      
-public class Solution {                                                                                                                                                      
-    public static void main(String[] args) {                                                                                                                                                      
-        //напишите тут ваш код                                                                                                       
-      System.out.print("Я думаю, быть программистом - это круто")    ;                                                                                                                                         
-    }                                                                                                                                                      
+package com.javarush.task.task01.task0101;
+
+/* 
+Я думаю, быть программистом круто
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        System.out.println("Я думаю, быть программистом круто");
+    }
 }
--- 1.JavaSyntax/src/com/javarush/task/task01/task0142/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0142/Solution.java
@@ -7,6 +7,9 @@ package com.javarush.task.task01.task0142;
 public class Solution {
     public static void main(String[] args) {
         //напишите тут ваш код
+<<<<<<< HEAD
         System.out.println("Амиго очень умный");
+=======
+>>>>>>> 927a3ad... Initial commit
     }
 }
