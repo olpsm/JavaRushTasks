c0c699a4a976e2a31dbd11536cccea12ea26d0ec
Oleg <olpsm@mail.ru>
2022-06-28 18:57:40 +0300

    added sandbox task0316

---
+++ sandbox/src/com/javarush/sandbox/tasks/task0316.java
@@ -0,0 +1,33 @@
+package com.javarush.sandbox.tasks;
+
+
+
+public class task0316 {
+/*
+Вывести на экран следующий текст - две строки:
+It's a Windows path: "C:\Program Files\Java\jdk1.7.0\bin"
+It's a Java string: \"C:\\Program Files\\Java\\jdk1.7.0\\bin\"
+
+Подсказка:
+\” – экранирование двойной кавычки;
+\\ – экранирование обратной косой черты (\).
+
+Больше про экранирование символов и Escape-последовательности в Java читай в статье.
+
+Требования:
+•	Программа должна выводить текст.
+•	Должно быть выведено две строки.
+•	Текст первый строки должен быть: It's a Windows path: "C:\Program Files\Java\jdk1.7.0\bin"
+•	Текст второй строки должен быть: It's a Java string: \"C:\\Program Files\\Java\\jdk1.7.0\\bin\"
+ * */
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("It's a Windows path: \"C:\\Program Files\\Java\\jdk1.7.0\\bin\"");
+        System.out.println("It's a Java string: \\\"C:\\\\Program Files\\\\Java\\\\jdk1.7.0\\\\bin\\\"");
+
+    }
+
+    }
+
+
+
