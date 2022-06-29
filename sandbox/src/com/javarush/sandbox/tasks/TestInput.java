b4007c33c91e2f41c68af41569719c660129cab4
Oleg <olpsm@mail.ru>
2022-06-29 18:54:11 +0300

    added sandbox task0325 and TestInput

---
+++ sandbox/src/com/javarush/sandbox/tasks/TestInput.java
@@ -0,0 +1,44 @@
+package com.javarush.sandbox.tasks;
+
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class TestInput {
+/*
+
+ * */
+
+    public static void main(String[] args) throws Exception {
+
+    while (true)
+    {
+           /* System.out.print("Your input for System.in:");
+            InputStream in = System.in;
+            int x = in.read();
+            // print System.in.read results (bytes)
+
+           // System.out.println("System.in.read() result:");
+            System.out.println(x);
+            //System.out.println("");
+*/
+            /*System.out.print("Your input for InputStream:");
+            InputStreamReader reader = new InputStreamReader(System.in);
+            int y = reader.read();
+            // print InputStreamReader.read results (symbol code)
+            System.out.println("InputStreamReader.read results:");
+            System.out.println(y);
+            System.out.println("");*/
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String s = reader.readLine();
+        System.out.println("Мы считали с клавиатуры эту строку:");
+        System.out.println(s);
+        }
+    }
+
+    }
+
+
+
---
+++ sandbox/src/com/javarush/sandbox/tasks/task0325.java
@@ -0,0 +1,31 @@
+package com.javarush.sandbox.tasks;
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class task0325 {
+/*
+Ввести с клавиатуры число n.
+Вывести на экран надпись "Я буду зарабатывать $n в час".
+
+Пример:
+Я буду зарабатывать $50 в час
+
+Требования:
+•	Программа должна выводить текст.
+•	Программа должна считывать данные с клавиатуры.
+•	Выведенный текст должен содержать введенное число n.
+•	Выведенный тест должен полностью соответствовать заданию.
+ * */
+
+    public static void main(String[] args) throws Exception {
+        String n = new BufferedReader(new InputStreamReader(System.in)).readLine();
+        System.out.println("Я буду зарабатывать "+ n + " в час");
+
+    }
+
+    }
+
+
+
